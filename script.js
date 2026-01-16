d3e397df92c77ac4a0bb2e241f492a8f1269e7aa
afizzgnteng <hafizrandikaputra@gmail.com>
2026-01-16 18:35:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "ngga hitam",
+    "kok gabisa??",
+    "harus ngaku nih?",
+    "apa ni gabisa",
+    "jadi yajid itam?",
+    "gmana ni?",
+    "jadi yajid itam?",
+    "maksaa bjirr",
+    "yajidd putihh!!",
+    "apaa nii gabisa anjay"
 ];
 
 let messageIndex = 0;
